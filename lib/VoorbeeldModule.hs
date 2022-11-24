65b1d30b973e608cc0aee57ca602dd42d9cda9c7
Lukas Barragan Torres <lukasbt@DESKTOP-HC2O2MM.localdomain>
2022-11-24 10:48:44 +0100

    minimale parser

--- lib/VoorbeeldModule.hs
+++ lib/VoorbeeldModule.hs
@@ -5,7 +5,7 @@ import Text.ParserCombinators.Parsec
 import System.Exit (exitFailure)
 import System.IO
 
-parse :: String -> IO JValue
+parse :: String -> IO (String, JValue)
 parse fileName = parseFromFile pText fileName >>= either report return
   where report err = exitFailure
 
@@ -23,18 +23,29 @@ newtype JObj a = JObj { fromJObj :: [(String, a)] }
     deriving (Eq, Ord, Show)
 
 data JValue = JString String
-            | JKey String
             | JNumber Double
             | JBool Bool
             | JNull
-            | JObject (JObj JValue)
-            | JArray (JAry JValue)
+            | JField (String, JValue)
+            | JObject [(String, JValue)]
+            | JArray  [JValue]
+            | JKey String
             deriving (Eq, Ord, Show)
 
 -- JSON tekst bestaat uit ofwel een object ofwel een array, met eventueel whitespace
-pText :: CharParser () JValue
+pText :: CharParser () (String, JValue)
 pText = spaces *> text <?> "JSON text"
-    where text = JObject <$> pObject <|> JArray <$> pArray
+    where text =  pField
+
+
+pField :: CharParser () (String,  JValue)
+pField  = (,) <$> (pKey <* char ':' <* spaces) <*> pValue
+
+pObject :: CharParser () [(String, JValue)]
+pObject = pSeries '{' pField '}'
+
+pArray :: CharParser () [JValue]
+pArray = pSeries '[' pValue ']'
 
 pValue :: CharParser () JValue
 pValue = value <* spaces
@@ -47,26 +58,26 @@ pValue = value <* spaces
             <?> "JSON value"
 
 pBool :: CharParser () Bool
-pBool = True <$ string "true"
+pBool = True  <$ string "true"
     <|> False <$ string "false"
 
 pValueChoice = value <* spaces
   where value = choice [ JString <$> pString
-                       , JKey    <$> pKey
                        , JNumber <$> pNumber
                        , JObject <$> pObject
                        , JArray  <$> pArray
                        , JBool   <$> pBool
+                       , JKey    <$> pKey
                        , JNull   <$ string "null"
                        ]
                 <?> "JSON value"
 
 pKey :: CharParser () String
-pKey = many $ char '\\' *> satisfy (`notElem` ":\"\\")
+pKey = many $ satisfy (`notElem` ":\"")
 
 pString :: CharParser () String
 pString = between (char '\"') (char '\"') (many jchar)
-    where jchar = char '\\' *> satisfy (`notElem` "\"\\")
+    where jchar = satisfy (`notElem` "\"")
 
 decode :: Char -> Char -> CharParser () Char
 decode c r = r <$ char c
@@ -82,9 +93,3 @@ pSeries :: Char -> CharParser () a -> Char -> CharParser () [a]
 pSeries left parser right = between (char left <* spaces) (char right) $
                         (parser <* spaces) `sepBy` (char ',' <* spaces)
 
-pObject :: CharParser () (JObj JValue)
-pObject = JObj <$> pSeries '{' p_field '}'
-    where p_field = (,) <$> (pKey <* char ':' <* spaces) <*> pValue
-
-pArray :: CharParser () (JAry JValue)
-pArray = JAry <$> pSeries '[' pValue ']'
