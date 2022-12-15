f24637eab6ad20754181e305d45dc5e48e853ef1
lbarraga <lukas.barragantorres@ugent.be>
2022-12-15 20:31:07 +0100

    render verderwerken

--- lib/RenderModule.hs
+++ lib/RenderModule.hs
@@ -9,6 +9,7 @@ import ParserModule (parseGameFile)
 import Text.Parsec (ParseError)
 import Data.Maybe (fromJust)
 import GHC.IO (unsafePerformIO)
+import PlayerModule (decreaseHp, retrieveItem)
 
 
 -- Framerate van het spel.
@@ -31,14 +32,36 @@ window = InWindow "Patience" (pxlWidth, pxlHeight) windowPosition
 
 -- Achtergrond kleur van het gloss venster
 backgroundColor :: Color
-backgroundColor = makeColorI 221 160 221 1
+backgroundColor = white
 
 initGame :: Game
-initGame = extractGame $ parseGameFile "levels/level2.txt" 
+initGame = extractGame $ parseGameFile "levels/level3.txt" 
 
 wallPic, emptyPic :: Picture
 wallPic  = png "assets/wall.png"
 emptyPic = png "assets/floor.png" 
+
+itemHolderInset, itemHolderSize, itemHolderSpace, inventoryWidth, inventoryHeight :: Float
+inventoryWidth = 700
+inventoryHeight = itemHolderSize + 2 * itemHolderInset
+itemHolderInset = 7
+itemHolderSize = 70
+itemHolderSpace = itemHolderSize + itemHolderInset
+
+inventoryBackGroundColor :: Color
+inventoryBackGroundColor = makeColorI 139 69 19 255
+
+itemHolderColor :: Color
+itemHolderColor = makeColorI 72 36 10 255
+
+inventoryBackground :: Picture
+inventoryBackground = Color inventoryBackGroundColor $ rectangleSolid inventoryWidth inventoryHeight
+
+itemHolder :: Picture
+itemHolder = Color itemHolderColor $ rectangleSolid itemHolderSize itemHolderSize
+
+actionPanelEmpty :: Picture
+actionPanelEmpty = Color black $ rectangleSolid 300 400
 
 -- ----------------------------------------------------------------------------
 --
@@ -48,6 +71,9 @@ emptyPic = png "assets/floor.png"
 png :: String -> Picture
 png = fromJust . unsafePerformIO . loadJuicyPNG
 
+itemToPath :: Item -> String
+itemToPath item = "assets/" ++ itemId item ++ ".png"
+
 extractGame :: Either ParseError Game -> Game
 extractGame (Right g) = g
 extractGame _ = undefined
@@ -55,25 +81,79 @@ extractGame _ = undefined
 co2Gloss :: Int -> Int -> (Float, Float)
 co2Gloss x y = (fromIntegral (x * 32), fromIntegral (y * 32))
 
+translateToGloss :: Int -> Int -> Picture -> Picture
+translateToGloss x y = uncurry translate (co2Gloss x y) 
+
+mapWithCoordinates :: [[a]] -> (a -> Int -> Int -> b) -> [b]
+mapWithCoordinates l f = [ f tile x y | (y, row) <- zip [0..] l, (x, tile) <- zip [0..] row] 
+
+-- f [a1, a2, a3, ...] -> [a1, f a2, f (f a3), ...]
+cumulateF :: (a -> a) -> [a] -> [a] 
+cumulateF f [] = []
+cumulateF f (x:xs) = x : cumulateF f (map f xs)
+
+-- Verschuift de eerste picture niet, de tweede met (x, y), de derde met (2x, 2y), ...
+translateCumulative :: Float -> Float -> [Picture] -> [Picture]
+translateCumulative x y = cumulateF (translate x y)
+
+extendToLength :: Int -> [a] -> [Maybe a]
+extendToLength n l = map Just l ++ replicate (n - length l) Nothing
+
 -- ----------------------------------------------------------------------------
 --
 -- ----------------------------------------------------------------------------
 
-renderTile :: Char -> Int -> Int -> Picture
-renderTile '.' x y = emptyPic
-renderTile '*' x y = wallPic
-renderTile 's' x y = wallPic
-renderTile 'e' x y = wallPic
-renderTile _   _ _ = error "no tile"
+renderTile :: Char -> Picture
+renderTile '.' = emptyPic
+renderTile '*' = wallPic
+renderTile 's' = emptyPic
+renderTile 'e' = emptyPic
+renderTile _   = error "no tile"
 
 renderLayout :: Layout -> Picture
-renderLayout layout = pictures $ map pictures pictures2d  
-    where pictures2d = [[uncurry translate (co2Gloss x y) (renderTile (layout !! y !! x) x y) | x <- [0..(width - 1)]] | y <- [0..(height - 1)]]
-          height = length layout 
-          width = length $ head layout 
+renderLayout layout = pictures $ mapWithCoordinates layout render
+    where render tile x y = translateToGloss x y (renderTile tile)
+
+renderItem :: Item -> Picture
+renderItem = png . itemToPath
+
+renderInventoryItems :: [Item] -> Picture
+renderInventoryItems items = pictures spacedOutItemHolders 
+    where spacedOutItemHolders = translateCumulative itemHolderSpace 0 itemPics
+          itemPics = map renderInventoryItem (extendToLength 9 items)
+
+
+renderInventoryItem :: Maybe Item -> Picture
+renderInventoryItem Nothing     = itemHolder 
+renderInventoryItem (Just item) = pictures [itemHolder, resizedItem]
+    where resizedItem = scale 2.1875 2.1875 (renderItem item) -- 70 / 32
+
+renderInventory :: [Item] -> Picture
+renderInventory items = pictures [inventoryBackground, inventoryItems] 
+    where inventoryItems = translate (-itemHolderSpace * 4) 0 (renderInventoryItems items)
+
+renderText :: String -> Picture
+renderText = color white . scale 0.1 0.1 . text
+
+functionDescription :: String -> Arguments -> String
+functionDescription "increasePlayerHp" (Ids [id])       = "Increase hp with " ++ id
+functionDescription "leave"            (Ids [])         = "Leave and do Nothing"
+functionDescription "decreaseHp"       (Ids [id1, id2]) = "Decrease hp of " ++ id1 ++ " with " ++ id2
+functionDescription "retrieveItem"     (Ids [id])       = "Retrieve " ++ id
+functionDescription "useItem"          (Ids [id])       = "Use " ++ id
+functionDescription name               _                = error "no description for function " ++ name
+
+renderAction :: Action -> Picture
+renderAction act = renderText (functionDescription functionName functionArgs)
+    where functionName = name (action act)
+          functionArgs = arguments (action act)
+
+renderActions :: [Action] -> Picture
+renderActions = pictures . translateCumulative 0 50 . map renderAction 
 
 renderGame :: Game -> Picture
-renderGame g = (scale 3 3 . renderLayout . layout . head .levels) initGame
+--renderGame g = renderActions ((entityActions . head . entities . head . levels) g) 
+renderGame g = pictures [actionPanelEmpty, renderActions ((entityActions . head . entities . head . levels) g)]
 
 -- stap in de game
 step :: Float -> Game -> Game
